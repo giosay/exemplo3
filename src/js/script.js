c0c65407c255a72802c28f5b321eb96a791a67d3
Giovanna <giosay@hotmail.com>
2025-03-28 11:42:39 -0300

    Declarações e variáveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// Declaração e variáveis
+var nome ="Fiap";
+console.log(nome)
+
+let idade =17;
+console.log(idade)
+
+const sobrenome ="Fiapinho"
+console.log(sobrenome)
+
+// Undefined
+
+let exemplo;
+console.log(exemplo)
+
+let exemplo1 =null;
+console.log(exemplo1)
+
+// Tipos de variáveis
